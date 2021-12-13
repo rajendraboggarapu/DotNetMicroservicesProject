0f5c58c3f9bd26641648c9012e776f0797ee4f85
J  V G Naga Rajendra Boggarapu <30888973+rajendraboggarapu@users.noreply.github.com>
2021-12-13 18:08:40 +0530

    catalogService with rabitmq

---
+++ src/CatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using System;
+using CatalogAPI.Entities;
+using CatalogAPI.Data;
+
+namespace CatalogAPI.Data
+{
+    public class CatalogContext:ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var mongoclient = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = mongoclient.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/CatalogAPI/Repositories/IproductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CatalogAPI.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Repositories
+{
+    public interface IproductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+
+    }
+}
